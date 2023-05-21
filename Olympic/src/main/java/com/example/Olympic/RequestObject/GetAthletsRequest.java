56eb6322f8210193dd82bea9d72a1e9952f1bfd9
unknown <fahima.alhabsi@gmail.com>
2023-05-21 15:47:04 +0400

    creating annotation for response and request

--- Olympic/src/main/java/com/example/Olympic/RequestObject/GetAthletsRequest.java
+++ Olympic/src/main/java/com/example/Olympic/RequestObject/GetAthletsRequest.java
@@ -1,4 +1,16 @@
 package com.example.Olympic.RequestObject;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetAthletsRequest {
+    private String name;
+    private String nationality;
+    private String sport;
 }
--- Olympic/src/main/java/com/example/Olympic/ResponseObject/GetAthletsResponse.java
+++ Olympic/src/main/java/com/example/Olympic/ResponseObject/GetAthletsResponse.java
@@ -1,4 +1,16 @@
 package com.example.Olympic.ResponseObject;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetAthletsResponse {
+    private String name;
+    private String nationality;
+    private String sport;
 }
