e461fb089c70634af5d43df08dac77d495209201
unknown <fahima.alhabsi@gmail.com>
2023-05-21 14:48:51 +0400

    creating function

--- Olympic/src/main/java/com/example/Olympic/Controller/AthletesController.java
+++ Olympic/src/main/java/com/example/Olympic/Controller/AthletesController.java
@@ -1,8 +1,10 @@
 package com.example.Olympic.Controller;
 
 
+import com.example.Olympic.Models.Athletes;
 import com.example.Olympic.Service.AthletsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
@@ -11,4 +13,19 @@ import javax.persistence.Entity;
 public class AthletesController {
     @Autowired
     AthletsService athletsService;
+
+@RequestMapping ("athletes/create")
+public void saveAthletes(){
+ createAthlets();
+
+
+}
+
+    private void createAthlets() {
+        Athletes athletes = new Athletes();
+        athletes.setNationality("omani");
+        athletes.setName("fahima");
+        athletes.setSport("football");
+    }
+
 }
--- Olympic/src/main/java/com/example/Olympic/Service/AthletsService.java
+++ Olympic/src/main/java/com/example/Olympic/Service/AthletsService.java
@@ -1,5 +1,6 @@
 package com.example.Olympic.Service;
 
+import com.example.Olympic.Models.Athletes;
 import com.example.Olympic.Repositry.AthletesRepositry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class AthletsService {
 
     @Autowired
     AthletesRepositry athletesRepositry;
+public void saveAthletes (Athletes athetes){
+    athletesRepositry.save(athetes);
+}
+
+
 }
