aa31451544215ecf89b7dc4f3235c0fb606cdba1
unknown <fahima.alhabsi@gmail.com>
2023-05-21 11:08:38 +0400

    generatedvalue for each class

--- Olympic/src/main/java/com/example/Olympic/Models/Events.java
+++ Olympic/src/main/java/com/example/Olympic/Models/Events.java
@@ -10,10 +10,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
 @Entity
 @Data
 public class Events {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
