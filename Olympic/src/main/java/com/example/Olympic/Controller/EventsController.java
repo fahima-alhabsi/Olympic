296212e6e8b3087d64f773f5717cb2a37177a920
U-Usaid <osaid.osaid66@gmail.com>
2023-05-25 10:06:59 +0400

    make creatoin

--- Olympic/src/main/java/com/example/Olympic/Controller/EventsController.java
+++ Olympic/src/main/java/com/example/Olympic/Controller/EventsController.java
@@ -1,12 +1,29 @@
 package com.example.Olympic.Controller;
 
+import com.example.Olympic.Models.Athletes;
+import com.example.Olympic.Models.Events;
 import com.example.Olympic.Service.EventsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class EventsController {
 
     @Autowired
     EventsService eventsService;
+    @RequestMapping("events/create")
+    public void saveevents() {
+        createevents();
+    }
+
+    private void createevents() {
+        Events events = new Events();
+        events.setEventName("olympic");
+        events.setSchedule(new Date());
+        events.setResults("win");
+        events.setSport("run");
+    }
 }
--- Olympic/src/main/java/com/example/Olympic/Service/EventsService.java
+++ Olympic/src/main/java/com/example/Olympic/Service/EventsService.java
@@ -1,11 +1,23 @@
 package com.example.Olympic.Service;
 
+import com.example.Olympic.Models.Athletes;
+import com.example.Olympic.Models.Events;
 import com.example.Olympic.Repositry.EventRepositry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EventsService {
     @Autowired
     EventRepositry eventRepositry;
+    public void saveevents (Events events){
+        eventRepositry.save(events);
+    }
+    public List<Events> getevents() {
+
+        return eventRepositry.findAll();
+    }
+
 }
